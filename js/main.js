dd9a55876e836a02d7b6bec8a70afbf57f0f4358
Anuj Sharma <49609133+Optider@users.noreply.github.com>
2020-10-25 18:07:41 +0530

    Add tab-detection script

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(window).on("blur focus", function(e) {
+    var prevType = $(this).data("prevType");
+
+    if (prevType != e.type) {   //  reduce double fire issues
+        switch (e.type) {
+            case "blur":
+                $('div').text("ALERT TEACHER!!! Student made unexpected attempt."); // modify text to display status
+                break;
+            case "focus":
+                $('div').text("Focused! Student is paying attention."); // modify text to display status
+                break;
+        }
+    }
+
+    $(this).data("prevType", e.type);
+})
+
